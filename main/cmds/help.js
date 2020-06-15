9c66a718366ed3c5e37bb14f025f54493800426d
Bill Xia <bill.j.xia@gmail.com>
2020-06-15 10:09:24 -0700

    added to help cmd

--- main/cmds/help.js
+++ main/cmds/help.js
@@ -2,7 +2,7 @@ function help() {
   return {
     color: 3447003,
     title: "Help",
-    description: "**command** (abbreviation) <required> [optional]\n**clantop** (**ct**) <clan tag> <ship name> [c for compact, leave empty for all stats]\n**graph** (**g**) [other user mention, default is author of message] <ship> [wr, dmg, kills, default is pr]",
+    description: "**command** (abbreviation) <required> [optional]\n**clantop** (**ct**) <clan tag> <ship name> [c for compact, leave empty for all stats]\n**graph** (**g**) [other user mention, default is author of message] <ship> [wr, dmg, kills, default is pr]\nWhen typing shipnames, exclude spaces and dashes.",
     footer: {
       text: `Command prefix: ${process.env.PREFIX}`
     }
